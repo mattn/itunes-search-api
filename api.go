a375987c680a8548dd803272e61171e2ccb0e5c8
Nick Wallace <nick@jackpotrising.com>
2017-05-30 19:39:32 -0500

    debugging

--- api.go
+++ api.go
@@ -118,10 +118,10 @@ func Lookup(search_term string, search_term_value string, entity string, limit i
 	var ret struct {
 		LookupResult []LookupResult
 	}
-	log.Println("BODY: ", res.Body)
 	err = json.NewDecoder(res.Body).Decode(&ret)
 	if err != nil {
 		return nil, err
 	}
+	log.Println("BODY: ", ret)
 	return ret.LookupResult, nil
 }
