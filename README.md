6ef3f745558e0dd794f5ed04600ed33912ed5806
Nick Wallace <nick@jackpotrising.com>
2017-05-30 19:02:16 -0500

    Added Lookup API and edited Search API Result Struct

---
+++ README.md
@@ -0,0 +1,14 @@
+# iTunes Search API
+iTunes Search API for Go.
+
+# Installation
+```golang
+go get github.com/Fyb3roptik/itunes-search-api
+```
+
+# Examples
+```golang
+import(
+  "github.com/Fyb3roptik/hubspot"
+)
+```
--- api.go
+++ api.go
@@ -6,7 +6,7 @@ import (
 	"net/url"
 )
 
-type Result struct {
+type SearchResult struct {
 	ArtistId               float64 `json:"artistId"`
 	ArtistName             string  `json:"artistName"`
 	ArtistViewUrl          string  `json:"artistViewUrl"`
@@ -40,7 +40,50 @@ type Result struct {
 	WrapperType            string  `json:"wrapperType"`
 }
 
-func Search(query, country, media string) ([]Result, error) {
+type LookupResult struct {
+	ArtistViewUrl												string		`json:"artistViewUrl"`
+	ArtworkUrl60												string		`json:"artworkUrl60"`
+	ArtworkUrl100												string		`json:"artworkUrl100"`
+	IpadScreenshotUrls									[]string	`json:"ipadScreenshotUrls"`
+	AppletvScreenshotUrls								[]string	`json:"appletvScreenshotUrls"`
+	ArtworkUrl512												[]string	`json:"artworkUrl512"`
+	IsGameCenterEnabled									bool			`json:"isGameCenterEnabled"`
+	Features														[]string	`json:"features"`
+	Kind																string		`json:"kind"`
+	SupportedDevices										[]string	`json:"supportedDevices"`
+	ScreenshotUrls											[]string	`json:"screenshotUrls"`
+	Advisories													[]string	`json:"advisories"`
+	TrackCensoredName										string		`json:"trackCensoredName"`
+	TrackViewUrl												string		`json:"trackViewUrl"`
+	ContentAdvisoryRating								string		`json:"contentAdvisoryRating"`
+	LanguageCodesISO2A									[]string	`json:"languageCodesISO2A"`
+	FileSizeBytes												string		`json:"fileSizeBytes"`
+	SellerUrl														string		`json:"sellerUrl"`
+	TrackContentRating									string		`json:"trackContentRating"`
+	Currency														string		`json:"currency"`
+	WrapperType													string		`json:"wrapperType"`
+	Version															string		`json:"version"`
+	ArtistId														int				`json:"artistId"`
+	ArtistName													string		`json:"artistName"`
+	Genres															[]string	`json:"genres"`
+	Price																float64		`json:"price"`
+	Description													string		`json:"description"`
+	TrackId															int				`json:"trackId"`
+	TrackName														string		`json:"trackName"`
+	BundleId														string		`json:"bundleId"`
+	ReleaseNotes												string		`json:"releaseNotes"`
+	PrimaryGenreName										string		`json:"primaryGenreName"`
+	IsVppDeviceBasedLicensingEnabled		bool			`json:"isVppDeviceBasedLicensingEnabled"`
+	ReleaseDate													string		`json:"releaseDate"`
+	FormattedPrice											string		`json:"formattedPrice"`
+	MinimumOsVersion										string		`json:"minimumOsVersion"`
+	PrimaryGenreId											int				`json:"primaryGenreId"`
+	SellerName													string		`json:"sellerName"`
+	GenreIds														[]string	`json:"genreIds"`
+	CurrentVersionReleaseDate						string		`json:"currentVersionReleaseDate"`
+}
+
+func Search(query, country, media string) ([]SearchResult, error) {
 	u := url.Values{}
 	u["term"] = []string{query}
 	u["country"] = []string{country}
@@ -51,11 +94,31 @@ func Search(query, country, media string) ([]Result, error) {
 	}
 	defer res.Body.Close()
 	var ret struct {
-		Results []Result
+		SearchResult []SearchResult
 	}
 	err = json.NewDecoder(res.Body).Decode(&ret)
 	if err != nil {
 		return nil, err
 	}
-	return ret.Results, nil
+	return ret.SearchResult, nil
+}
+
+// New Lookup API
+// search_term will be id, amgArtistId, upc, isbn
+func Lookup(search_term string, search_term_value string, entity string, limit int, sort string) ([]LookupResult, error) {
+	u := url.Values{}
+	u[search_term] = []string{search_term_value}
+	res, err := http.Get("https://itunes.apple.com/lookup?" + u.Encode())
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	var ret struct {
+		LookupResult []LookupResult
+	}
+	err = json.NewDecoder(res.Body).Decode(&ret)
+	if err != nil {
+		return nil, err
+	}
+	return ret.LookupResult, nil
 }
